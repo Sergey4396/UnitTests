23f68659f050fd86baf276a3d501eb16bf8d9614
Sergey <s.s.r@mail.ru>
2020-05-29 18:57:48 +0300

    added mainPresenter

--- UnitTests/UnitTests/MainPresentor.swift
+++ UnitTests/UnitTests/MainPresentor.swift
@@ -13,7 +13,26 @@ protocol MainViewProtocol: class {
 }
 
 protocol MainViewPresenterProtocol: class {
-    init(view:MainViewProtocol, person:Person) {
+    init(view:MainViewProtocol, person:Person)
     func showGreeting()
-    }
+    
 }
+
+class MainPresenter: MainViewPresenterProtocol {
+    let view: MainViewProtocol
+    let person: Person
+    
+    required init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    func showGreeting() {
+        let greeting = self.person.firstName + " " + self.person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+    
+
+}
+
+
